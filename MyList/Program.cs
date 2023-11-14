2614993675257715ec8f74ed8b75d6b37c10bd50
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-11-14 18:02:07 +0300

    Adding MyList Project

---
+++ MyList/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+List<string> BirListe = new();
+
+BirListe.Add("deneme2");
+BirListe.Add("deneme2");
+BirListe.Add("deneme2");
+
+Console.WriteLine("Liste1: " + BirListe.Count);
+
+MyList<string> BenimListem = new MyList<string>();
+
+BenimListem.Add("Deneme");
+BenimListem.Add("Deneme");
+BenimListem.Add("Deneme");
+BenimListem.Add("Deneme");
+BenimListem.Add("Deneme");
+
+Console.WriteLine("Liste2: " + BenimListem.Count);
+
+Console.ReadKey();
+
+class MyList<T>
+{
+    T[] _array;
+
+
+    public MyList()
+    {
+        _array = new T[0];
+    }
+
+    public void Add(T item)
+    {
+        _array = new T[_array.Length + 1];
+    }
+
+
+    public int Count
+
+    {
+        get { return _array.Length; }
+
+    }
+}
+
+
